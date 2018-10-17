31408fb29ba83b73f04d8bc0a4f431e26efe1781
Windy <liuzhyun126@gmail.com>
2018-10-17 22:04:50 +0800

    配置文件以及日志输出相关模块学习

--- README.md
+++ README.md
@@ -17,8 +17,19 @@ npm start
 ```
 ```
 
-### 当前学习内容
+### 工程结构
 
-1. express
+app.js：        启动文件，或者说入口文件
+package.json：  存储着工程的信息及模块依赖，当在 dependencies 中添加依赖的模块时，运行npm install，npm 会检查当前目录下的 package.json，并自动安装所有指定的模块
+node_modules：  存放 package.json 中安装的模块，当你在 package.json 添加依赖的模块并安装后，存放在这个文件夹下
+public：        存放 image、css、js 等文件
+routes：        存放路由文件
+views：         存放视图文件或者说模版文件
+bin：           存放可执行文件
 
 
+### 学习笔记
+
+1. [配置文件模块工具 config-lite](https://github.com/sondragon/node-study/blob/master/notes/%E9%85%8D%E7%BD%AE%E5%B7%A5%E5%85%B7config-lite.md)
+2. [日志中间件 morgan](https://github.com/expressjs/morgan)
+3. [日志文件每日新建一份存储 rotating-file-stream](https://github.com/iccicci/rotating-file-stream)
--- app.js
+++ app.js
@@ -1,2 +1,55 @@
 
-console.log('Hello World!');
+const express = require('express');
+const path = require('path');
+const config = require('config-lite')(__dirname);
+const logger = require('morgan');
+const fs = require('fs');
+const rfs = require('rotating-file-stream');
+
+// 生成一个 express 实例
+const app = express();
+
+// 指定网页渲染模板
+app.set('views', path.join(__dirname, 'views')); // 设置 views 文件夹为存放视图文件的目录
+app.set('view engine', 'ejs'); // 设置视图模板引擎 ejs
+
+var logDirectory = path.join(__dirname, 'logs');
+
+// ensure log directory exists
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
+
+function pad(num) {
+    return (num > 9 ? "" : "0") + num;
+}
+
+// create a rotating write stream
+var accessLogStream = rfs((time, index) => {
+        if(! time)
+            return "file.log";
+
+        var month  = time.getFullYear() + "" + pad(time.getMonth() + 1);
+        var day    = pad(time.getDate());
+        var hour   = pad(time.getHours());
+        var minute = pad(time.getMinutes());
+
+        return month + "/" + month + day + "-" + hour + minute + "-" + index + "-file.log";
+    }, {
+        interval: '1d', // rotate daily
+        path: logDirectory,
+        initialRotation: true
+});
+
+// 自定义输出日志格式
+logger.format('combined2', ':remote-addr - :remote-user [:date[iso]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"');
+
+app.use(logger('combined2', { stream: accessLogStream })); // 加载日志中间件-输出到文件
+app.use(logger('dev')); // 加载日志中间件
+app.use(express.json()); // 加载解析 json 的中间件
+app.use(express.urlencoded({ extended: false })); // 加载解析urlencoded请求体的中间件
+//app.use(cookieParser()); // 加载解析 cookie 的中间件 - 需另外安装
+app.use(express.static(path.join(__dirname, 'public'))); // 设置public文件夹为存放静态文件的目录
+
+
+app.listen(config.port, function(){
+console.log(`Server running at port: ${config.port}...`);
+})
